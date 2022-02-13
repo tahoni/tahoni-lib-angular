15f17b40629d4968aaa88e42e18ed6816acce7a8
tahoni <tahoni@gmail.com>
2022-02-13 16:34:34 +0200

    Fix copyright component

--- projects/tahoni-lib/src/lib/copyright/copyright.component.ts
+++ projects/tahoni-lib/src/lib/copyright/copyright.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CopyrightService } from './copyright.service';
 
 @Component({
@@ -6,10 +6,12 @@ import { CopyrightService } from './copyright.service';
   templateUrl: './copyright.component.html',
   styleUrls: ['./copyright.component.scss'],
 })
-export class CopyrightComponent {
+export class CopyrightComponent implements OnInit {
   copyrightService: CopyrightService;
 
   constructor(copyrightService: CopyrightService) {
     this.copyrightService = copyrightService;
   }
+
+  ngOnInit(): void {}
 }
